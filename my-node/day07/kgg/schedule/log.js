7ac1e25bef5df033359af560c4c46a97306409a7
evaplane <18676724845@163.com>
2021-03-10 14:00:17 +0800

    修改vue

--- my-node/day07/kgg/schedule/log.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	// corntab 6个占位符从左到右分别代表：秒、分、时、⽇、⽉、周⼏, '
-	interval:'*/3 * * * * *',
-	handler(){
-		console.log('定时任务 3秒一次' + new Date());
-	}
-}
--- my-node/day12/security/01/app/config.js
+++
@@ -1,8 +0,0 @@
-modeule.export = {
-    user: "root",
-    password: '123456',
-    database: 'test',
-    host: 'localhost',
-    port: 3306
-
-}
