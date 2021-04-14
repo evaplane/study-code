10ea2411eed337d78f21804e58099793f9ce316d
evaplane <18676724845@163.com>
2021-04-14 12:46:50 +0800

    删除文件

--- vue/vue3--kkb/vue3-movie/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-// use(store)所以可以敲出$store
-createApp(App).use(router).use(store).mount('#app')
