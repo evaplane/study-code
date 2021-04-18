92ea4cc9996e228c533bbf20415877e700674bef
evaplane <18676724845@163.com>
2021-04-18 23:18:42 +0800

    新建register页面

---
+++ front/pages/register.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="register-form">
+    <el-form> </el-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- server/app/controller/util.js
+++ server/app/controller/util.js
@@ -1,20 +1,21 @@
+'use strict'
 const Controller = require('egg').Controller
 const svgCaptcha = require('svg-captcha')
 class UtilController extends Controller {
   async captcha() {
-   const captcha = svgCaptcha.create(
-	   {
-		   size:4,
-		   fontSize:50,
-		   width:100,
-		   height:40,
-		   noise:3
-	   }
-   )
-   // 存入session
-   this.ctx.session.captcha = captcha.text;
-   this.ctx.response.type="image/svg+xml";
-   this.ctx.body = captcha.data
+    const captcha = svgCaptcha.create(
+      {
+        size: 4,
+        fontSize: 50,
+        width: 100,
+        height: 40,
+        noise: 3,
+      }
+    )
+    // 存入session
+    this.ctx.session.captcha = captcha.text
+    this.ctx.response.type = 'image/svg+xml'
+    this.ctx.body = captcha.data
   }
 }
 
